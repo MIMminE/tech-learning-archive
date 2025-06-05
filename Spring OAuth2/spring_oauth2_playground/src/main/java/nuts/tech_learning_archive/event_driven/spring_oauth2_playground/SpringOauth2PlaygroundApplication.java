277c1e6d307b0813e2860fbd6811928a10a8d91b
MIMminE <weluv.ww@gmail.com>
2025-06-05 15:12:50 +0900

    [ Spring OAuth2 ] archive init

---
+++ Spring OAuth2/spring_oauth2_playground/src/main/java/nuts/tech_learning_archive/event_driven/spring_oauth2_playground/SpringOauth2PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package nuts.tech_learning_archive.event_driven.spring_oauth2_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2PlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2PlaygroundApplication.class, args);
+    }
+
+}
---
+++ Spring OAuth2/spring_oauth2_playground/src/test/java/nuts/tech_learning_archive/event_driven/spring_oauth2_playground/SpringOauth2PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package nuts.tech_learning_archive.event_driven.spring_oauth2_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2PlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
