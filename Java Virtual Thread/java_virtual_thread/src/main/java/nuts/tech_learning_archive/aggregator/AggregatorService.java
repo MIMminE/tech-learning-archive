4500752cf88ed79a437cf91d2823ac2fca924bbb
MIMminE <weluv.ww@gmail.com>
2025-07-22 13:01:54 +0900

    [ Virtual Thread ] ~ Timeout

--- Java Virtual Thread/java_virtual_thread/src/main/java/nuts/tech_learning_archive/aggregator/AggregatorService.java
+++ Java Virtual Thread/java_virtual_thread/src/main/java/nuts/tech_learning_archive/aggregator/AggregatorService.java
@@ -2,9 +2,10 @@ package nuts.tech_learning_archive.aggregator;
 
 import nuts.tech_learning_archive.sec7.ExternalServiceClient;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class AggregatorService {
 
@@ -15,8 +16,17 @@ public class AggregatorService {
     }
 
     public ProductDto getProductDto(int id) throws ExecutionException, InterruptedException {
-        var product = executor.submit(() -> ExternalServiceClient.getProduct(id));
-        var rating = executor.submit(() -> ExternalServiceClient.getRating(id));
-        return new ProductDto(id, product.get(), rating.get());
+//        var product = executor.submit(() -> ExternalServiceClient.getProduct(id));
+        var product = CompletableFuture.supplyAsync(() -> ExternalServiceClient.getProduct(id), executor)
+                .exceptionally(ex -> "product-not-found");
+
+
+//        var rating = executor.submit(() -> ExternalServiceClient.getRating(id));
+        var rating = CompletableFuture.supplyAsync(() -> ExternalServiceClient.getRating(id), executor)
+                .exceptionally(ex -> 1)
+                .orTimeout(750, TimeUnit.MILLISECONDS)
+                .exceptionally(ex -> 2);
+
+        return new ProductDto(id, product.join(), rating.join());
     }
 }
